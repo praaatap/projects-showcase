e43148bc72983ebe28dcb79c61e61abe5db28375
Your Name <raghurajramkumar927@gmail.com>
2025-11-01 19:33:24 +0530

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<div>About Page</div>} />
+    </Routes>
+  )
+}
